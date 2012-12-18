ccf4f6622572c9eca1db692ffa8802949b34eb74
nomadic-house <rich@nomadichouse.co.uk>
2012-12-18 16:13:16 +0000

    added webhook files from syncapp

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,6 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :price
-
-  def self.new_from_shopify(shop, shopify_id)
-
-      shopify_p = ShopifyAPI::Product.find(shopify_id)
-
-      # creates the local product
-      local_p = Product.new
-      local_p.shopify_id = shopify_p.id
-      local_p.name = shopify_p.title
-      local_p.shop = shop
-      local_p.save
-
-  end
+  validates_uniqueness_of :shopify_id
   
+  has_many :webhook_events
+  belongs_to :shop
 end
