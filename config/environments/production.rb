fe5dec93a791a6c60fc7ac8a1f461585ab29cc09
nomadic-house <rich@nomadichouse.co.uk>
2012-12-18 18:50:31 +0000

    dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,8 @@ Licensesync::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+   config.action_dispatch.x_sendfile_header = "X-Sendfile"
+
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -19,6 +21,8 @@ Licensesync::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.i18n.fallbacks = true
+    config.active_support.deprecation = :notify
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
