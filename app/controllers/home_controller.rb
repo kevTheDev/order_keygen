47fdb7e904fb2300cb922d2ba64e5520579c8259
nomadic-house <rich@nomadichouse.co.uk>
2012-12-18 18:43:50 +0000

    product var change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -25,7 +25,7 @@ ShopifyAPI::Base.ssl_options = {:ssl_version => :TLSv1}
   
   def index
     # get 5 products
-    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+    @productsnh = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
 
     # get latest 5 orders
     @orders   = ShopifyAPI::Order.find(:all, :params => {:limit => 10, :order => "created_at DESC" })
