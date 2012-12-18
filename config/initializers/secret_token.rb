4d8da0064961e325c777f60750a3e871fff2de74
nomadic-house <rich@nomadichouse.co.uk>
2012-12-18 13:39:02 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Licensesync::Application.config.secret_token = 'b418ece6081ae348c677f62a8642c9647f1607b6cea4ecb51bcca37dce87187f5acddbf4c0195a536c9652ac4f17372da57d2629f21c2ddd8cb51457e66988f1'
