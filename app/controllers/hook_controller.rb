ed135d8c1d9bca00bedd0136c4b4c8eac571ad29
nomadic-house <rich@nomadichouse.co.uk>
2013-01-06 23:45:37 +0000

    master

--- app/controllers/hook_controller.rb
+++ app/controllers/hook_controller.rb
@@ -18,6 +18,11 @@ ShopifyAPI::Base.ssl_options = {:ssl_version => :TLSv1}
 
 
   around_filter :shopify_session, :except => 'welcome'
+ 
+  def welcome
+    current_host = "#{request.host}#{':' + request.port.to_s if request.port != 80}"
+    @callback_url = "http://#{current_host}/login"
+  end
   
   
   def index
