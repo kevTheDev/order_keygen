652165326fbd80d4eddbf56074c3b59a5e9d113f
nomadic-house <rich@nomadichouse.co.uk>
2013-01-06 23:54:04 +0000

    login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,5 @@
 class HomeController < ApplicationController
 
-  require 'active_resource/connection'
-
-class ActiveResource::Connection
-  def apply_ssl_options_with_ssl_version(http)
-    apply_ssl_options_without_ssl_version(http)
-
-    http.ssl_version = @ssl_options[:ssl_version] if @ssl_options[:ssl_version]
-
-    http
-  end
-
-  alias_method_chain :apply_ssl_options, :ssl_version
-end
-
-ShopifyAPI::Base.ssl_options = {:ssl_version => :TLSv1}
   
   around_filter :shopify_session, :except => 'welcome'
   
