f96964eac7f5ed3a7f1c848728173c8b317d28c0
nomadic-house <rich@nomadichouse.co.uk>
2013-01-17 11:23:26 +0000

    shared_secret

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,6 +3,7 @@ class WebhookController < ApplicationController
 
 around_filter :shopify_session, :except => 'welcome'
 before_filter :verify_webhook, :except => 'verify_webhook'
+@SHARED_SECRET = '5ff673736415fce868a3c0df89cbfd51'
 
  def welcome
     current_host = "#{request.host}#{':' + request.port.to_s if request.port != 80}"
@@ -91,14 +92,13 @@ puts "Decoded: #{data}"
   
   def verify_webhook
     data = request.body.read.to_s
-    puts "Decoded from verify: #{data} \n"
-    SHARED_SECRET = 5ff673736415fce868a3c0df89cbfd51 
+    puts "Decoded from verify: #{data} \n" 
     hmac_header = request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
       puts "Header: #{hmac_header} \n"
     digest  = OpenSSL::Digest::Digest.new('sha256')
 
     puts "Digest: #{digest} \n"
-    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, SHARED_SECRET, data)).strip
+    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, @SHARED_SECRET, data)).strip
       puts "Calc Header: #{calculated_hmac} \n"
     unless calculated_hmac == hmac_header
       head :unauthorized
