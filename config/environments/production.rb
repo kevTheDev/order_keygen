5ccb1494790188b607e73b2fbe2b6d79064cc185
nomadic-house <rich@nomadichouse.co.uk>
2013-01-17 08:54:10 +0000

    rails_log_stdout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,6 @@ Licensesync::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.logger = Logger.new(STDOUT) 
-  config.log_level = :info
-
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
