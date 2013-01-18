feed209287a9fbc80bb42abf7e7cde5d5f775dd8
nomadic-house <rich@nomadichouse.co.uk>
2013-01-18 00:07:19 +0000

    test

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ActiveRecord::Base
   
   has_many :webhook_events
   belongs_to :shop
+  attr_accessible :name
+  attr_accessible :price
+  attr_accessible :shopify_id
+  attr_accessible :shop_id
 end
