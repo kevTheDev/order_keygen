42a10a7304c2c3bee3f340880bbc87b5c0461df4
nomadic-house <rich@nomadichouse.co.uk>
2013-01-18 00:12:47 +0000

    fresh

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,7 @@ class Shop < ActiveRecord::Base
   validates_uniqueness_of :name
   has_many :products
   has_many :webhook_events
+  attr_accessible :name
+  attr_accessible :url
+  attr_accessible :installed
 end
--- app/models/webhook_event.rb
+++ app/models/webhook_event.rb
@@ -3,4 +3,6 @@ class WebhookEvent < ActiveRecord::Base
   belongs_to :shop
   attr_accessible :event_type
   attr_accessible :description
+  attr_accessible :product_id
+  attr_accessible :shop_id
 end
