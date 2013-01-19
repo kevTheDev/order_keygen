4e3c6e749d77b6a90542157495efa3467f82be25
nomadic-house <rich@nomadichouse.co.uk>
2013-01-19 23:25:36 +0000

    filter

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -3,7 +3,7 @@ class WebhookController < ApplicationController
 require 'base64'
 require 'openssl'
 
-around_filter :shopify_session, :except => 'verify_webhook'
+skip_around_filter :shopify_session
 before_filter :verify_webhook, :except => 'verify_webhook'
 
 
