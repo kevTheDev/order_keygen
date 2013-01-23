412da1706509220dc82bce05d91bb6fad5aad6c6
kevTheDev <kev.j.edwards@gmail.com>
2013-01-23 13:16:44 +0000

    Added permission to read orders

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            ShopifyApp.configuration.secret,
 
            # Example permission scopes - see http://api.shopify.com/authentication.html for full listing
-          :scope => 'write_orders, write_products, write_customers',
+          :scope => 'write_orders, write_products, write_customers, read_orders',
 
            :setup => lambda {|env| 
                        params = Rack::Utils.parse_query(env['QUERY_STRING'])
