88df867de7ba8e63445228c931dbc739098b258e
kevTheDev <kev.j.edwards@gmail.com>
2013-01-23 10:50:32 +0000

    Setup base shopify app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderKeygen::Application.config.secret_token = 'b113ec5f52bf763f11870b9981d8632d411780a0a604d4b4aa1fa7964f040718b423ca094fe08943846afd4563ef0da3ef739b73140cb9b4c02cc1c00d9308e5'
