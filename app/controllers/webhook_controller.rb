1155615ca825555c4df05adbcca98d26b2121adf
kevTheDev <kev.j.edwards@gmail.com>
2013-01-24 13:25:11 +0000

    Use shared secret from config

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -48,9 +48,8 @@ class WebhookController < ApplicationController
     hmac_header = request.headers['HTTP_X_SHOPIFY_HMAC_SHA256']
     puts "Header: #{hmac_header} \n"
     digest  = OpenSSL::Digest::Digest.new('sha256')
-    @SHARED_SECRET = '5ff673736415fce868a3c0df89cbfd51'
     puts "Digest: #{digest} \n"
-    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, @SHARED_SECRET, data)).strip
+    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, Licensesync::Application.config.shopify.secret, data)).strip
     puts "Calc Header: #{calculated_hmac} \n"
     
     unless calculated_hmac == hmac_header
